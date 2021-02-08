5004b3b4a61bcad1f006151fd30e8a4a3adf3feb
raais <raaisnattey@gmail.com>
2021-02-08 13:06:15 +0500

    source image no downscale

--- add.py
+++ add.py
@@ -21,7 +21,7 @@ def cap():
         face_locations = face_recognition.face_locations(rgb_small_frame)
         face_encodings = face_recognition.face_encodings(rgb_small_frame)[0]
 
-        cv2.imwrite('source/source.jpg', rgb_small_frame)
+        cv2.imwrite('source/source.jpg', frame)
 
     except IndexError:
         cap()
